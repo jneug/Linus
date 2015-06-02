6e5ef6edec3da5d264a359a397c774b9bb521c19
Jonas Neugebauer <jneug@zitmail.upb.de>
2015-06-02 12:23:28 +0200

    Some major new styles
    - Added styles for MW forms
    - Added styles for SMW
    - Improvements to overall styles

--- js/linus.js
+++ js/linus.js
@@ -1,5 +1,8 @@
 $(function() {
     // Some general styling of elements
+    // Fix inverted footer color for html-tag
+    $('body.inverted-navbar').parent().addClass('inverted-navbar');
+
     // Buttons
     $('input[type=submit],input[type=button],input[type=reset]').addClass('btn btn-default');
     $('input[type=submit]').addClass('btn-primary').removeClass('btn-default');
@@ -9,7 +12,7 @@ $(function() {
     // Form on edit pages
     if ($('#wpTextbox1')) {
         // Cancel link (buttons are styled by hook)
-        $('.editButtons').addClass('well'); // TODO: Move to LinusHook?
+        // $('.editButtons').addClass('well'); // TODO: Move to LinusHooks or even remove?
         //$('span.cancelLink a').addClass('btn btn-warning');
         $('#mw-editform-cancel').addClass('btn btn-warning');
     }
@@ -17,39 +20,53 @@ $(function() {
     // Links in alerts
     $('.alert a').addClass('alert-link');
 
+    // Preferences tabs
+    $('#preftoc').addClass('nav nav-pills');
+
     // Move toc
-    if ($('#toc') && $('#sidebar')) {
-        $('#toc')
-			.prependTo($('#sidebar'))
-			.affix();
-    }
+    if( $('#sidebar') ) {
+        if ($('#toc')) {
+            $('#toc')
+        			.prependTo($('#sidebar'))
+        			.affix();
 
-    // Activate Smooth scrolling
-    if( $('body.smooth-scroll') ) {
-        $('a[href*=#]:not([href=#])').click(function() {
-            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-              var target = $(this.hash);
-              target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-              if (target.length) {
-                $('html,body').animate({
-                  scrollTop: target.offset().top - 50
-              }, 800);
-                return false;
+              // Activate Smooth scrolling
+              if( $('body.smooth-scroll') ) {
+                $('#toc a[href*=#]:not([href=#])').click(function() {
+                    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                      var target = $(this.hash);
+                      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                      if (target.length) {
+                        $('html,body').animate({
+                          scrollTop: target.offset().top - 50
+                      }, 800);
+                        return false;
+                      }
+                    }
+                });
               }
-            }
-        });
-    }
-
-    // remove sidebar if empty
-    if( $('#sidebar').children().length == 0 ) {
-        $('#sidebar').remove();
-        $('#main-content')
-            .removeClass('col-md-9')
-            .addClass('col-md-12');
+        } else
+        // remove sidebar if empty
+        if( $('#sidebar').children().length == 0 ) {
+            $('#sidebar').remove();
+            $('#main-content')
+                .removeClass('col-md-9')
+                .addClass('col-md-12');
+        }
     }
 
     // Some SMW styling
-    $('.smw-editpage-help').addClass('well').removeClass('smw-editpage-help');
+    $('.smw-editpage-help')
+        .removeClass('smw-editpage-help')
+        .addClass('well');
+    if( $('.smwfact') ) {
+        $('.smwfact')
+            .removeClass('smwfact')
+            .addClass('smw-factbox');
+        $('.smwfacttable')
+            .removeClass('smwfacttable')
+            .addClass('table table-condensed table-bordered table-striped table-hover');
+    }
 
     // Initialize BS components
     // Init tooltips and popovers
