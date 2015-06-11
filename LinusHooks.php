191e271bd15bd65063588b6c51ddd17d949dc8e6
Jonas Neugebauer <jneug@zitmail.upb.de>
2015-06-11 12:29:53 +0200

    New layout for category pages
    The other two variants (based on the BS grid and the `columns` CSS property) are still available.

--- LinusHooks.php
+++ LinusHooks.php
@@ -36,8 +36,8 @@ class LinusHooks {
     global $wgLinusResponsiveCategories;
     if ( $wgLinusResponsiveCategories && $title->getNamespace() == NS_CATEGORY ) {
        $article = new LinusCategoryPage( $title );
-   }
-   return true;
+    }
+    return true;
   }
 
 	static function styleEditButtons( &$editpage, &$buttons, &$tabindex ) {
@@ -96,12 +96,15 @@ class LinusHooks {
 }
 
 class LinusCategoryPage extends CategoryPage {
-   protected $mCategoryViewerClass = 'LinusCategoryViewer';
+  protected $mCategoryViewerClass = 'LinusCategoryViewer';
 }
 
 class LinusCategoryViewer extends CategoryViewer {
+  public static $mCategoryLayout = 'rowLayout';
+
   function formatList( $articles, $articles_start_char, $cutoff = 6 ) {
-    $list = $this->columnLayout( $articles, $articles_start_char );
+    // $list = $this->columnLayout( $articles, $articles_start_char );
+    $list = call_user_func_array(array($this, self::$mCategoryLayout), array($articles, $articles_start_char));
 
     $pageLang = $this->title->getPageLanguage();
     $attribs = array( 'lang' => $pageLang->getCode(), 'dir' => $pageLang->getDir(),
@@ -111,7 +114,10 @@ class LinusCategoryViewer extends CategoryViewer {
     return $list;
   }
 
-  function columnLayout( $articles, $articles_start_char ) {
+  /**
+   * Render category items in three grid columns.
+   */
+  function gridLayout( $articles, $articles_start_char ) {
     $columns = array_combine( $articles, $articles_start_char );
 		# Split into three columns
 		$columns = array_chunk( $columns, ceil( count( $columns ) / 3 ), true /* preserve keys */ );
@@ -160,5 +166,78 @@ class LinusCategoryViewer extends CategoryViewer {
 
 		$ret .= '</div>';
 		return $ret;
+  }
+
+  /**
+   * render category items in a .col-md-12 with the columns css property applied
+   */
+  function columnLayout( $articles, $articles_start_char ) {
+    $size = count($articles);
+    $half = ceil( $size / 2 );
+    $third = ceil( $size / 3 );
+
+    $ret = '<div class="row"><div class="col-md-12"><div class="catcolumns">'."\n";
+    $prevchar = null;
+
+    foreach( $articles as $i=>$article) {
+      $char = $articles_start_char[$i];
+      # Change space to non-breaking space to keep headers aligned
+      $h3char = $char === ' ' ? '&#160;' : htmlspecialchars( $char );
+
+      if( $i === 0 || $char !== $prevchar ) {
+        $ret .= "<h3>".$h3char."</h3>\n";
+      } else {
+        if( $i%$half === 0 ) {
+          $ret .= "<h3 class=\"visible-sm-block\">"
+                    .$h3char
+                    .' ' . wfMessage( 'listingcontinuesabbrev' )->escaped()
+                    ."</h3>\n";
+        }
+        else if( $i%$third === 0 ) {
+          $ret .= "<h3 class=\"visible-md-block visible-lg-block\">"
+                    .$h3char
+                    .' ' . wfMessage( 'listingcontinuesabbrev' )->escaped()
+                    ."</h3>\n";
+        }
+      }
+
+      $ret .= $article."<br/>\n";
+
+      $prevchar = $char;
+    }
+
+    $ret .= '</div></div></div>';
+    return $ret;
+  }
+
+  /**
+   * Render category items in rows after their respective header
+   */
+  function rowLayout( $articles, $articles_start_char ) {
+    $ret = "";
+    $prevchar = null;
+
+    $first = true;
+    foreach( $articles as $i=>$article) {
+      $char = $articles_start_char[$i];
+      # Change space to non-breaking space to keep headers aligned
+      $h3char = $char === ' ' ? '&#160;' : htmlspecialchars( $char );
+
+      if( $i === 0 || $char !== $prevchar ) {
+        if( !$first ) {
+          $ret .= '</div>';
+        }
+        $ret .= '<div class="row catrow">'."\n";
+        $ret .= "<div class=\"col-lg-12\"><h3>".$h3char."</h3></div>\n";
+      }
+
+      $ret .= '<div class="col-lg-3 col-md-4 col-sm-6">'.$article."</div>\n";
+
+      $first = false;
+      $prevchar = $char;
+    }
+
+    $ret .= "</div>";
+    return $ret;
   }
 }
