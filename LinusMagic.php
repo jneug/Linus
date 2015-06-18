d52e355a95eeeb7a480ced4ee7d57fbab9275455
Jonas Neugebauer <jneug@zitmail.upb.de>
2015-06-18 07:48:48 +0200

    Missing file for magic words

---
+++ LinusMagic.php
@@ -0,0 +1,9 @@
+<?php
+
+$magicWords = array();
+
+/** English (English) */
+$magicWords['en'] = array(
+  'LINUS_NOSIDEBAR'   => array( 0, '__NOSIDEBAR__' ),
+  'LINUS_NOTITLE'     => array( 0, '__NOTITLE__' ),
+);
