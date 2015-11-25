e9e515fbd3c0a2318c24c90207b52abc1bd45441
Jonas Neugebauer <jneug@zitmail.upb.de>
2015-11-25 09:36:40 +0100

    Formatting

--- LinusMagic.php
+++ LinusMagic.php
@@ -4,7 +4,7 @@ $magicWords = array();
 
 /** English (English) */
 $magicWords['en'] = array(
-  'LINUS_NOSIDEBAR'   => array( 0, '__NOSIDEBAR__' ),
+  'LINUS_NOSIDEBAR'      => array( 0, '__NOSIDEBAR__' ),
   'LINUS_FORCESIDEBAR'   => array( 0, '__FORCESIDEBAR__' ),
-  'LINUS_NOTITLE'     => array( 0, '__NOTITLE__' ),
+  'LINUS_NOTITLE'        => array( 0, '__NOTITLE__' ),
 );
